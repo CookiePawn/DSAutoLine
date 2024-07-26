3a2c44ba631254a77ddbe0898e1630f221ebdadb
minhyuksim <als3954@naver.com>
2024-07-26 13:27:14 +0900

    DSAL-13
    1. 이벤트 카드배치 생성

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -2,14 +2,13 @@ import React, { useState } from 'react';
 import Footer from "../components/Footer";
 import GNB from "../components/GNB";
 import '../styles/EventPage.css';
-import { EventCard, EventCardIndicator } from '../components/Cards';
+import {EventCard} from '../components/Cards';
 
 const EventPage = (props) => {
     const [selectedButton, setSelectedButton] = useState(0);
     const [list, setList] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
-    const [eventCurrentIndex, setEventCurrentIndex] = useState(0);
     const [eventStart, setEventStart] = useState(0);
-    const [eventEnd, setEventEnd] = useState(3);
+    const [eventEnd, setEventEnd] = useState(10);
 
     return (
         <>
@@ -19,7 +18,7 @@ const EventPage = (props) => {
                     <h1>이벤트/프로모션</h1>
                     <p>더 많은 혜택과 함께 하세요</p>
                 </div>
-                <div className="eventStatus">
+                <div className="eventSection">
                     <div className="eventButtons">
                         <button
                             onClick={() => setSelectedButton(0)}
@@ -34,16 +33,11 @@ const EventPage = (props) => {
                             종료된 이벤트
                         </button>
                     </div>
-                    <div className='eventListDiv'>
+                    <div className='eventList'>
                         {list.slice(eventStart, eventEnd).map((item, idx) => (
                             <EventCard key={idx} item={item} />
                         ))}
                     </div>
-                    <EventCardIndicator
-                        list={list}
-                        currentIndex={eventCurrentIndex}
-                        setCurrentIndex={setEventCurrentIndex}
-                    />
                 </div>
             </div>
             <Footer />
