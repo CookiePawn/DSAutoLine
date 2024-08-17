873441ae256fb29db7b73052530511d6bd721e7f
minhyuksim <als3954@naver.com>
2024-08-17 20:38:23 +0900

    DSAL -6
    1. 카카오맵 API연동 후 사이트 업로드

--- src/pages/EnterPage.js
+++ src/pages/EnterPage.js
@@ -1,21 +1,52 @@
-import GNB from "../components/GNB"
+import React, { useEffect } from 'react';
+import GNB from "../components/GNB";
 import Footer from "../components/Footer";
 import '../styles/EnterPage.css';
-import DSAutoLine from '../assets/img/dsautoline/DSAUTOLINE.png'
+import DSAutoLine from '../assets/img/dsautoline/DSAUTOLINE.png';
 
-const EnterPage = (props) => {
+const EnterPage = () => {
+
+    useEffect(() => {
+        const mapAPI = process.env.REACT_APP_MAP_API;
+
+        const script = document.createElement('script');
+        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${mapAPI}&autoload=false`;
+        script.async = true;
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            if (window.kakao && window.kakao.maps) {
+                window.kakao.maps.load(() => {
+                    const container = document.getElementById('map');
+                    const options = {
+                        center: new window.kakao.maps.LatLng(36.83187125070437, 127.14499934927386),
+                        level: 1
+                    };
+                    const map = new window.kakao.maps.Map(container, options);
+                    const markerPosition = new window.kakao.maps.LatLng(36.831875766482334, 127.14499095041695);
+                    const marker = new window.kakao.maps.Marker({
+                        position: markerPosition
+                    });
+                    marker.setMap(map);
+                    
+                });
+            }
+        };
+
+    }, []);
+
     return (
-        <>
-            <GNB stat={true} page={'회사소개'}/>
+        <div>
+            <GNB stat={true} page={'회사소개'} />
             <div className="EnterbannerSection">
                 <h1>고객님의 <span className="Point">‘마음’</span>을 움직입니다.</h1>
                 <p>저희 DS 오토라인은 신뢰와 믿음을 바탕으로 고객님의 마음을 움직이겠습니다.</p>
             </div>
             <div className="EnterTitleSection">
-                <img src={DSAutoLine} className="titlelogo" alt="이미지 로딩이 실패하였습니다"/>
+                <img src={DSAutoLine} className="titlelogo" alt="이미지 로딩이 실패하였습니다" />
                 <p>렌트/리스 분야에서 여러 경험과 노하우, 신뢰와 믿음을 바탕으로 고객님들의 마음을 움직이는</p>
                 <p>컨설팅을 진행해 드리는 자동차 금융서비스 전문 기업입니다.</p>
-                <p>다양한 파트너 사와 직,간접적인 업무 진행을 통해 유통 마진을 줄이고 고객 최우선적 1대1 심</p>
+                <p>다양한 파트너 사와 직, 간접적인 업무 진행을 통해 유통 마진을 줄이고 고객 최우선적 1대1 심</p>
                 <p>층상담을 통해 보다 합리적이고 어디에서 쉽게 볼 수 없는 견적서를 드리겠습니다.</p>
             </div>
             <div className="advantageSection">
@@ -25,19 +56,18 @@ const EnterPage = (props) => {
                         <li>1. 낮은 초기비용 or 유지비용</li>
                         <li>2. 유지 관리의 편리</li>
                         <li>3. 개인 사업자 or 법인 사업자 비용처리 가능</li>
-                        <li>4. 신용도에 영향 x</li>
+                        <li>4. 신용도에 영향 없음</li>
                     </ul>
                 </div>
                 <div className="advantage-column">
-                    <h1>리스장점</h1>
+                    <h1>리스 장점</h1>
                     <ul className="listsytle">
                         <li>1. 낮은 초기비용</li>
-                        <li>2. 건강보험료, 재산세 할증 부담x</li>
+                        <li>2. 건강보험료, 재산세 할증 부담 없음</li>
                         <li>3. 개인 사업자 or 법인 사업자 비용처리 가능</li>
                         <li>4. 보험경력 유지</li>
                     </ul>
                 </div>
-                
             </div>
             <div className="processSection">
                 <h1>계약진행절차</h1>
@@ -60,14 +90,13 @@ const EnterPage = (props) => {
             </div>
             <div className="GuideSection">
                 <h1>DS오토라인 오시는 길</h1>
-                <div className="GuideMapSection">
-                    <h1>맵 공간</h1>
+                <div className='GuideMapSection'>
+                    <div id="map"></div>
                 </div>
             </div>
             <Footer />
-        
-        </>
-    )
+        </div>
+    );
 }
 
 export default EnterPage;
