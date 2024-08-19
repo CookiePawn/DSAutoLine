33694f259a7353a79552e40bac5b5daf3c3ce2d1
minhyuksim <als3954@naver.com>
2024-08-19 18:17:28 +0900

    DSAL -6
    1.회사 소개 페이지 새로 수정

--- src/pages/EnterPage.js
+++ src/pages/EnterPage.js
@@ -4,7 +4,6 @@ import Footer from "../components/Footer";
 import '../styles/EnterPage.css';
 import DSAutoLine from '../assets/img/dsautoline/DSAUTOLINE.png';
 import FastFAQSticky from '../components/FastFAQSticky'
-import { FaS } from 'react-icons/fa6';
 
 const EnterPage = () => {
 
@@ -46,65 +45,166 @@ const EnterPage = () => {
             <GNB stat={true} page={'회사소개'} />
             <FastFAQSticky height={550}/>
             <div className='enter_container'>
-                <div className="EnterbannerSection">
-                    <div>
-                        <h1>고객님의 <span className="Point">‘마음’</span>을 움직입니다.</h1>
-                        <p>저희 DS 오토라인은 신뢰와 믿음을 바탕으로 고객님의 마음을 움직이겠습니다.</p>
+
+                <div className='Enter_VISIONSection'>
+                    <div className='Enter_TitleHead'>
+                        <p>VISION</p>
+                    </div>
+                    <div className='Enter_TitleTextSeciton'>
+                        <div className='Enter_TitleText'>
+                            <h1>DS AUTOLINE은<br/> 고객님의 마음을 움직입니다.</h1>
+                            <p>신뢰와 믿음을 바탕으로<br/> 고객님의 마음을 움직이겠습니다.</p>
+                        </div>
                     </div>
                 </div>
-                <div className="EnterTitleSection">
-                    <img src={DSAutoLine} className="titlelogo" alt="이미지 로딩이 실패하였습니다" />
-                    <p>렌트/리스 분야에서 여러 경험과 노하우, 신뢰와 믿음을 바탕으로 고객님들의 마음을 움직이는</p>
-                    <p>컨설팅을 진행해 드리는 자동차 금융서비스 전문 기업입니다.</p>
-                    <p>다양한 파트너 사와 직, 간접적인 업무 진행을 통해 유통 마진을 줄이고 고객 최우선적 1대1 심</p>
-                    <p>층상담을 통해 보다 합리적이고 어디에서 쉽게 볼 수 없는 견적서를 드리겠습니다.</p>
-                </div>
-                <div className="advantageSection">
-                    <div>
-                        <div className="advantage-column">
-                            <h1>장기렌트 장점</h1>
-                            <ul className="listsytle">
-                                <li>1. 낮은 초기비용 or 유지비용</li>
-                                <li>2. 유지 관리의 편리</li>
-                                <li>3. 개인 사업자 or 법인 사업자 비용처리 가능</li>
-                                <li>4. 신용도에 영향 없음</li>
-                            </ul>
+                <div className='Enter_bannerSection'/>  
+                <div className='Enter_line'></div>
+
+                <div className='Enter_HOWSection'>
+                    <div className='Enter_TitleHead'>
+                        <p>HOW</p>
+                    </div>
+                    <div className='Enter_TitleTextSeciton'>
+                        <div className='Enter_TitleText'>
+                            <h1>DS AUTOLINE은<br/> 자동차 금융 서비스 전문 기업입니다.</h1>
+                            <p>렌트, 리스 분야에서 여러 경험과 노하우, 신뢰와 믿음을<br/> 바탕으로 컨설팅을 진행합니다.</p>
                         </div>
-                        <div className="advantage-column">
-                            <h1>리스 장점</h1>
-                            <ul className="listsytle">
-                                <li>1. 낮은 초기비용</li>
-                                <li>2. 건강보험료, 재산세 할증 부담 없음</li>
-                                <li>3. 개인 사업자 or 법인 사업자 비용처리 가능</li>
-                                <li>4. 보험경력 유지</li>
-                            </ul>
+                    </div>
+                    <div className='Enter_HOWTextSection'>
+                        <h1>고객님께 이런 서비스를 제공하겠습니다.</h1>
+                        <p>- 다양한 파트너사와 직접적, 간접적인 업무 진행을 통하여 유통 마진을 줄이겠습니다.</p>
+                        <p>- 고객 최우선적 1대1 심층 상담을 통하여 보다 합리적이고 어디에서 쉽게 볼 수 없는 견적서를 드리겠습니다.</p>
+                    </div>
+                    <div className='Enter_line'></div>
+                </div>
+                <div className='Enter_RENTSection'>
+                    <div className='Enter_TitleHead'>
+                        <p>RENT</p>
+                    </div>
+                    <div className='Enter_TitleTextSeciton'>
+                        <div className='Enter_TitleText'>
+                            <h1>DS AUTOLINE의<br/> 장기렌트는 이런 장점이 있습니다.</h1>
                         </div>
                     </div>
 
-                </div>
-                <div className="processSection">
-                    <h1>계약진행절차</h1>
-                    <div className="circleSection">
-                        <div className="circle">리스/렌트 상담</div>
-                        <div className="line"></div>
-                        <div className="circle">견적서 발송</div>
-                        <div className="line"></div>
-                        <div className="circle">필요서류 준비</div>
-                        <div className="line"></div>
-                        <div className="circle">심사 및 승인</div>
+                    <div className='Enter_RENTTextSection'>
+                        <div className='Enter_RENTColumnHead'>
+                            <div className='Enter_RENTColumnText'>
+                                <h1><span>01</span> 낮은 초기비용과 유지 비용</h1>
+                                <p>낮은 초기 비용과 유지 비용을 통하여 고객님들께서 부담을<br/>덜 가지실 수 있도록 해드립니다.</p>
+                            </div>
+                            <div className='Enter_RENTColumnText'>
+                                <h1><span>02</span> 유지 관리의 편의</h1>
+                                <p>고객님들께서 차량을 받으신 후에도 유지 관리에 신경을 덜 쓰실 수 있도록<br/>서비스를 지원하고 있습니다.</p>
+                            </div>
+                        </div>
+                        <div className='Enter_RENTColumnHead'>
+                            <div className='Enter_RENTColumnText'>
+                                <h1><span>03</span> 개인 사업자, 법인 사업자 비용 처리 가능</h1>
+                                <p>개인 사업자 고객님과 법인 사업자 고객님들을 모두 생각하여<br/>사업자 비용을 처리하실 수 있도록 지원하고 있습니다.</p>
+                            </div>
+                            <div className='Enter_RENTColumnText'>
+                                <h1><span>04</span> 신용도에 영향 X</h1>
+                                <p>고객님께서 저희 서비스를 사용을 하여 차량을 받으셔도<br/>고객님의 신용도에는 전혀 영향이 없습니다.</p>
+                            </div>
+                        </div>
+                        <div className='Enter_bannerSection'/> 
                     </div>
-                    <div className="circleSection">
-                        <div className="circle">계약 완료</div>
-                        <div className="line"></div>
-                        <div className="circle">차량 출고</div>
-                        <div className="line"></div>
-                        <div className="circle">사후관리</div>
-                    </div>
+                    <div className='Enter_line'></div>
                 </div>
-                <div className="GuideSection">
-                    <h1>DS오토라인 오시는 길</h1>
-                    <div className='GuideMapSection'>
-                        <div id="map"></div>
+
+                <div className='Enter_LEASESection'>
+                    <div className='Enter_TitleHead'>
+                        <p>LEASE</p>
+                    </div>
+                    <div className='Enter_TitleTextSeciton'>
+                        <div className='Enter_TitleText'>
+                            <h1>DS AUTOLINE의<br/> 리스는 이런 장점이 있습니다.</h1>
+                        </div>
+                    </div>
+                    <div className='Enter_RENTTextSection'>
+                        <div className='Enter_RENTColumnHead'>
+                            <div className='Enter_RENTColumnText'>
+                                <h1><span>01</span> 낮은 초기비용과 유지 비용</h1>
+                                <p>낮은 초기 비용과 유지 비용을 통하여 고객님들께서 부담을<br/>덜 가지실 수 있도록 해드립니다.</p>
+                            </div>
+                            <div className='Enter_RENTColumnText'>
+                                <h1><span>02</span> 건강보험료, 재산세 할증 부담 X</h1>
+                                <p>고객님의 건강보험료와 재산세에 대한 할증을 부담하지 않으셔도 됩니다.</p>
+                            </div>
+                        </div>
+                        <div className='Enter_RENTColumnHead'>
+                            <div className='Enter_RENTColumnText'>
+                                <h1><span>03</span> 보험 경력 유지</h1>
+                                <p>고객님께서 서비스를 이용하신 후에도 보험 경력을 변경되지 않고<br/>기존 고객님의 보험 경력이 그대로 유지됩니다.</p>
+                            </div>
+                            <div className='Enter_RENTColumnText'>
+                                <h1><span>04</span> 개인 사업자, 법인 사업자 비용 처리 가능</h1>
+                                <p>개인 사업자 고객님과 법인 사업자 고객님들을 모두 생각하여<br/>사업자 비용을 처리하실 수 있도록 지원하고 있습니다.</p>
+                            </div>
+                        </div>
+                        <div className='Enter_bannerSection'/> 
+                    </div>
+                    <div className='Enter_line'></div>
+                    
+                <div className='Enter_PROCESSSection'>
+                    <div className='Enter_TitleHead'>
+                        <p>PROCEDURE</p>
+                    </div>
+                    <div className='Enter_TitleTextSeciton'>
+                        <div className='Enter_TitleText'>
+                            <h1>DS AUTOLINE은<br/> 계약 진행 절차는 다음과 같습니다.</h1>
+                        </div>
+                    </div>
+                    <div className='Enter_Process'>
+                        <div className='process-item'>
+                            <img src='/processcheck.png' alt='Process Check' className='process-icon'/>
+                            <p>렌트, 리스 상담을 통한 견적서 발송</p>
+                        </div>
+                        <div className='process-item'>
+                            <img src='/processcheck.png' alt='Process Check' className='process-icon'/>
+                            <p>필요 서류 준비</p>
+                        </div>
+                        <div className='process-item'>
+                            <img src='/processcheck.png' alt='Process Check' className='process-icon'/>
+                            <p>심사 및 승인</p>
+                        </div>
+                        <div className='process-item'>
+                            <img src='/processcheck.png' alt='Process Check' className='process-icon'/>
+                            <p>계약 완료</p>
+                        </div>
+                        <div className='process-item'>
+                            <img src='/processcheck.png' alt='Process Check' className='process-icon'/>
+                            <p>인도지 출고</p>
+                        </div>
+                        <div className='process-item'>
+                            <img src='/processcheck.png' alt='Process Check' className='process-icon'/>
+                            <p>사후 관리</p>
+                        </div>
+                    </div>
+                </div>  
+                <div className='Enter_line'></div>
+                </div>
+
+                <div className='Enter_WHERESection'>
+                    <div className='Enter_TitleHead'>
+                        <p>WHERE</p>
+                    </div>
+                    <div className='Enter_TitleTextSeciton'>
+                        <div className="GuideSection">
+                            <h1>DS오토라인 오시는 길</h1>
+                            <div className='GuideMapSection'>
+                                <div id="map"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <div className='Enter_line'></div>
+                <div className='Enter_appliSection'>
+                    <div className='Enter_TitleTextSeciton'>
+                        <div className='Enter_TitleText'>
+                            <h1>DS AUTOLINE에<br/> 한번 상담 신청해보세요.</h1>
+                        </div>
                     </div>
                 </div>
             </div>
