bfcf560137abe31cfcc0bb3292ba94eea2d3345f
CookiePawn <cookiepawn@naver.com>
2024-09-19 13:31:08 +0900

    DSAL-14
    1. 이벤트 페이지 상담 신청 PC 작성

--- src/pages/EventDetailPage.js
+++ src/pages/EventDetailPage.js
@@ -73,40 +73,47 @@ const EventDetailPage = () => {
                             e.target.src = `${process.env.REACT_APP_IMG_URL}/error.png`;
                         }}
                     />
+                    {!content.title.includes('퍼스트스테이') &&
+                        <span>
+                            <span>
+                                <div className='eventDetailPage_FastFAQ'>
+                                    <img src={require('../assets/img/dsautoline/DSAUTOLINE.png')} />
+                                    <p>이름</p>
+                                    <input value={name} onChange={(e) => setName(e.target.value)} placeholder='ex) 홍길동' />
+                                    <p>연락처</p>
+                                    <input
+                                        value={phone}
+                                        onChange={(e) => setPhone(e.target.value)}
+                                        maxLength={11}
+                                        placeholder='ex) 01012345678'
+                                        type='number'
+                                    />
+                                    <span>
+                                        <span>
+                                            {
+                                                !infoSelect1
+                                                    ? <img style={{ width: 23, height: 23 }} src={require('../assets/img/functionIcon/optionPage_nonSelectBox.png')} alt="Select Box" onClick={() => setInfoSelect1(!infoSelect1)} />
+                                                    : <img style={{ width: 23, height: 23 }} src={require('../assets/img/functionIcon/optionPage_SelectBox.png')} alt="Selected Box" onClick={() => setInfoSelect1(!infoSelect1)} />
+                                            }
+                                            <p><span>(필수)</span> 개인정보 제 3자 제공 동의 <span onClick={() => { setIsUsePopupVisible(true); document.body.style.overflowY = 'hidden' }}>[보기]</span></p>
+                                        </span>
+                                        <span>
+                                            {
+                                                !infoSelect2
+                                                    ? <img style={{ width: 23, height: 23 }} src={require('../assets/img/functionIcon/optionPage_nonSelectBox.png')} alt="Select Box" onClick={() => setInfoSelect2(!infoSelect2)} />
+                                                    : <img style={{ width: 23, height: 23 }} src={require('../assets/img/functionIcon/optionPage_SelectBox.png')} alt="Selected Box" onClick={() => setInfoSelect2(!infoSelect2)} />
+                                            }
+                                            <p><span>(필수)</span> 개인정보 수집ㆍ이용ㆍ제공 동의 <span onClick={() => { setIsUsePopupVisible(true); document.body.style.overflowY = 'hidden' }}>[보기]</span></p>
+                                        </span>
+                                    </span>
+                                    <button onClick={clickFunction}>상담하기</button>
+                                </div>
+                            </span>
+                        </span>
+                    }
                 </div>
                 {content.title.includes('퍼스트스테이') &&
                     <button onClick={() => window.location.href = 'https://first-stay.co.kr/'}>퍼스트스테이 바로가기</button>
-                }
-                {!content.title.includes('퍼스트스테이') &&
-                    <>
-                        <div className='eventDetailPage_FasrFAQ'>
-                            <input value={name} onChange={(e) => setName(e.target.value)} placeholder='ex) 홍길동' />
-                            <input
-                                value={phone}
-                                onChange={(e) => setPhone(e.target.value)}
-                                maxLength={11}
-                                placeholder='ex) 01012345678'
-                                type='number'
-                            />
-                            <span>
-                                {
-                                    !infoSelect1
-                                        ? <img style={{ width: 23, height: 23 }} src={require('../assets/img/functionIcon/optionPage_nonSelectBox.png')} alt="Select Box" onClick={() => setInfoSelect1(!infoSelect1)} />
-                                        : <img style={{ width: 23, height: 23 }} src={require('../assets/img/functionIcon/optionPage_SelectBox.png')} alt="Selected Box" onClick={() => setInfoSelect1(!infoSelect1)} />
-                                }
-                                <p><span>(필수)</span> 개인정보 제 3자 제공 동의 <span onClick={() => { setIsUsePopupVisible(true); document.body.style.overflowY = 'hidden' }}>[보기]</span></p>
-                            </span>
-                            <span>
-                                {
-                                    !infoSelect2
-                                        ? <img style={{ width: 23, height: 23 }} src={require('../assets/img/functionIcon/optionPage_nonSelectBox.png')} alt="Select Box" onClick={() => setInfoSelect2(!infoSelect2)} />
-                                        : <img style={{ width: 23, height: 23 }} src={require('../assets/img/functionIcon/optionPage_SelectBox.png')} alt="Selected Box" onClick={() => setInfoSelect2(!infoSelect2)} />
-                                }
-                                <p><span>(필수)</span> 개인정보 수집ㆍ이용ㆍ제공 동의 <span onClick={() => { setIsUsePopupVisible(true); document.body.style.overflowY = 'hidden' }}>[보기]</span></p>
-                            </span>
-                            <button onClick={clickFunction}>상담하기</button>
-                        </div>
-                    </>
                 }
                 <div className='ButtonSection'>
                     <button className="backButton" onClick={() => window.location.href = '/event'}>목록으로</button>
