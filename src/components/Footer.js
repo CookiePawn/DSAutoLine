7c38a3945347f8ed9e3fe39a93dfcded8ab2f238
CookiePawn <cookiepawn@naver.com>
2024-12-02 23:14:02 +0900

    Revert "13"
    This reverts commit 1a9061b984887cf71a1f08bf22840dc6e6c4d994.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -45,6 +45,19 @@ const Footer = (props) => {
             <div>
                 <p>COPYRIGHT @2024 DSAUTOLINE CO, LTD ALL RIGHTS RESERVED.</p>
             </div>
+            
+            
+                <script type="text/javascript" src="//wcs.naver.net/wcslog.js"> </script> 
+                <script type="text/javascript"> 
+                if (!wcs_add) var wcs_add={};
+                wcs_add["wa"] = "s_54bd969202cb";
+                if (!_nasa) var _nasa={};
+                if(window.wcs){
+                wcs.inflow();
+                wcs_do();
+                }
+                </script>
+
                     </footer>
                 )
             }
